f2091902ff7e2fd4062a8c4cd62f433b82d128e4
jerony <jerony_cruz@ucne.edu.do>
2025-05-08 18:50:36 -0400

    Creando Entidad

---
+++ app/src/main/java/edu/ucne/registrotecnico/data/local/entities/Tecnico.kt
@@ -0,0 +1,13 @@
+package edu.ucne.registrotecnico.data.local.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Tecnicos")
+
+data class TecnicoEntity(
+    @PrimaryKey
+    val tecnicoId: Int? = null,
+    val nombre: String = "",
+    val sueldo: Double
+    )
