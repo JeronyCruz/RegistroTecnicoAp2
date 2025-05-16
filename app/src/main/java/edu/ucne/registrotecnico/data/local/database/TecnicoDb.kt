17d157cf49a5cab8d9b7e920515d8750980658e1
jerony <jerony_cruz@ucne.edu.do>
2025-05-16 16:44:25 -0400

    Add new Entities.

--- app/src/main/java/edu/ucne/registrotecnico/data/local/database/TecnicoDb.kt
+++ app/src/main/java/edu/ucne/registrotecnico/data/local/database/TecnicoDb.kt
@@ -2,17 +2,25 @@ package edu.ucne.registrotecnico.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import edu.ucne.registrotecnico.data.local.dao.PrioridadDao
 import edu.ucne.registrotecnico.data.local.dao.TecnicoDao
+import edu.ucne.registrotecnico.data.local.dao.TicketDao
+import edu.ucne.registrotecnico.data.local.entities.PrioridadEntity
 import edu.ucne.registrotecnico.data.local.entities.TecnicoEntity
+import edu.ucne.registrotecnico.data.local.entities.TicketEntity
 
 @Database(
     entities = [
-        TecnicoEntity::class
+        TecnicoEntity::class,
+        PrioridadEntity::class,
+        TicketEntity::class
     ],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
 
 abstract  class TecnicoDb : RoomDatabase(){
     abstract fun TecnicoDao(): TecnicoDao
+    abstract  fun PrioridadDao(): PrioridadDao
+    abstract fun TicketDao(): TicketDao
 }
