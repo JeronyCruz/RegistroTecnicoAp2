441e79f61a9ba90bafcf399c2f4d9f117cb747a6
jerony <jerony_cruz@ucne.edu.do>
2025-05-17 01:03:29 -0400

    Add: Nuevo decorador para el almacenamiento de la fecha en la BD.

--- app/src/main/java/edu/ucne/registrotecnico/data/local/database/TecnicoDb.kt
+++ app/src/main/java/edu/ucne/registrotecnico/data/local/database/TecnicoDb.kt
@@ -2,12 +2,27 @@ package edu.ucne.registrotecnico.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
 import edu.ucne.registrotecnico.data.local.dao.PrioridadDao
 import edu.ucne.registrotecnico.data.local.dao.TecnicoDao
 import edu.ucne.registrotecnico.data.local.dao.TicketDao
 import edu.ucne.registrotecnico.data.local.entities.PrioridadEntity
 import edu.ucne.registrotecnico.data.local.entities.TecnicoEntity
 import edu.ucne.registrotecnico.data.local.entities.TicketEntity
+import java.util.Date
+
+class Converters {
+    @TypeConverter
+    fun fromTimestamp(value: Long?): Date? {
+        return value?.let { Date(it) }
+    }
+
+    @TypeConverter
+    fun dateToTimestamp(date: Date?): Long? {
+        return date?.time
+    }
+}
 
 @Database(
     entities = [
@@ -19,6 +34,7 @@ import edu.ucne.registrotecnico.data.local.entities.TicketEntity
     exportSchema = false
 )
 
+@TypeConverters(Converters::class)
 abstract  class TecnicoDb : RoomDatabase(){
     abstract fun TecnicoDao(): TecnicoDao
     abstract  fun PrioridadDao(): PrioridadDao
