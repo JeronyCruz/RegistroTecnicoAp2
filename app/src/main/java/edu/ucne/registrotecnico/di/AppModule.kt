58209d3da1845ec7c613154f362d95d454662cc3
jerony <jerony_cruz@ucne.edu.do>
2025-05-23 03:07:05 -0400

    Se creo el AppModule.

---
+++ app/src/main/java/edu/ucne/registrotecnico/di/AppModule.kt
@@ -0,0 +1,33 @@
+package edu.ucne.registrotecnico.di
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.registrotecnico.data.local.database.TecnicoDb
+import javax.inject.Singleton
+
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideTecnicoDb(@ApplicationContext appContext: Context) =
+        Room.databaseBuilder(
+            appContext,
+            TecnicoDb::class.java,
+            "Tecnico.db"
+        ).fallbackToDestructiveMigration()
+            .build()
+
+    @Provides
+    fun provideTecnicoDao(tecnicoDb: TecnicoDb) = tecnicoDb.TecnicoDao()
+    @Provides
+    fun providePrioridadDao(tecnicoDb: TecnicoDb) = tecnicoDb.PrioridadDao()
+    @Provides
+    fun provideTicketDao(tecnicoDb: TecnicoDb) = tecnicoDb.TicketDao()
+}
