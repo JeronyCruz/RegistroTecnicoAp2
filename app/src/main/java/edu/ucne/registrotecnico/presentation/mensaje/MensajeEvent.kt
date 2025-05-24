771375fa42d5eb037620ea43fb87753b0630c635
jerony <jerony_cruz@ucne.edu.do>
2025-05-24 13:10:08 -0400

    Se agrego un nuevo campo al MensajeEvent.kt.

--- app/src/main/java/edu/ucne/registrotecnico/presentation/mensaje/MensajeEvent.kt
+++ app/src/main/java/edu/ucne/registrotecnico/presentation/mensaje/MensajeEvent.kt
@@ -7,6 +7,7 @@ sealed interface MensajeEvent {
     data class MensajeChange(val mensajeId: Int): MensajeEvent
     data class FechaChange(val fecha: Date): MensajeEvent
     data class ContenidoChange(val contenido: String): MensajeEvent
+    data class tipoRemitenteChange(val tipoRemitente: String): MensajeEvent
     data class RemitenteChange(val remitente: String): MensajeEvent
     data class TicketIdChange(val ticketId: Int): MensajeEvent
     data object Save: MensajeEvent
