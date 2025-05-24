4ae4062588dbe53eba0d7a03a0914b6e51931740
jerony <jerony_cruz@ucne.edu.do>
2025-05-24 03:09:33 -0400

    Creacion de MensajeEvent.

---
+++ app/src/main/java/edu/ucne/registrotecnico/presentation/mensaje/MensajeEvent.kt
@@ -0,0 +1,15 @@
+package edu.ucne.registrotecnico.presentation.mensaje
+
+import edu.ucne.registrotecnico.presentation.prioridades.PrioridadEvent
+import java.util.Date
+
+sealed interface MensajeEvent {
+    data class MensajeChange(val mensajeId: Int): MensajeEvent
+    data class FechaChange(val fecha: Date): MensajeEvent
+    data class ContenidoChange(val contenido: String): MensajeEvent
+    data class RemitenteChange(val remitente: String): MensajeEvent
+    data class TicketIdChange(val ticketId: Int): MensajeEvent
+    data object Save: MensajeEvent
+    data object Delete: MensajeEvent
+    data object New: MensajeEvent
+}
