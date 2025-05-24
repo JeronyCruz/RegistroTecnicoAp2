438e68dd964bb25355eeb87909fd4c7326ff22cd
jerony <jerony_cruz@ucne.edu.do>
2025-05-24 03:09:06 -0400

    Creacion de MensajeDao.

---
+++ app/src/main/java/edu/ucne/registrotecnico/data/local/dao/MensajeDao.kt
@@ -0,0 +1,26 @@
+package edu.ucne.registrotecnico.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import edu.ucne.registrotecnico.data.local.entities.MensajeEntity
+import edu.ucne.registrotecnico.data.local.entities.PrioridadEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface MensajeDao {
+    @Upsert()
+    suspend fun save(Mensaje: MensajeEntity)
+    @Query("""
+            SELECT *
+                FROM Mensajes
+                WHERE mensajeId =:id
+                limit 1
+    """)
+    suspend fun find(id: Int): MensajeEntity?
+    @Delete
+    suspend fun delete(Mensaje: MensajeEntity)
+    @Query("SELECT * FROM Mensajes Where ticketId =:ticketId")
+    fun getAll(ticketId: Int): Flow<List<MensajeEntity>>
+}
