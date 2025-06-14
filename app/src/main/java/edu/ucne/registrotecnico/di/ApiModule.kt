6ddca485eb33afc94f6aa6cca982289fd2ab59c9
jerony <jerony_cruz@ucne.edu.do>
2025-06-14 00:52:14 -0400

    Se creo ApiModule.

---
+++ app/src/main/java/edu/ucne/registrotecnico/di/ApiModule.kt
@@ -0,0 +1,36 @@
+package edu.ucne.registrotecnico.di
+
+import com.squareup.moshi.KotlinJsonAdapterFactory
+import com.squareup.moshi.Moshi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.registrotecnico.data.remote.VehiculoinApi
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object ApiModule {
+    const val BASE_URL = "https://vehiculosapi.azurewebsites.net/"
+
+    @Provides
+    @Singleton
+    fun providesMoshi(): Moshi =
+        Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .add(DateAdapter())
+            .build()
+
+    @Provides
+    @Singleton
+    fun providesVehiculoApi(moshi: Moshi): VehiculoinApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(VehiculoinApi::class.java)
+    }
+}
