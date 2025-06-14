b216beff3eaa74a069058c82d49fb197b38ee531
jerony <jerony_cruz@ucne.edu.do>
2025-06-14 00:55:36 -0400

    Se creo el VehiculoUiState.

---
+++ app/src/main/java/edu/ucne/registrotecnico/presentation/vehiculo/VehiculoUiState.kt
@@ -0,0 +1,15 @@
+package edu.ucne.registrotecnico.presentation.vehiculo
+
+import edu.ucne.registrotecnico.data.remote.dto.VehiculoDto
+
+data class VehiculoUiState(
+    val vehiculoId: Int? = null,
+    val descripcion: String = "",
+    val errorDescripcion: String? = null,
+    val errorMessage: String? = null,
+    val precio: Double = 0.0,
+    val errorPrecio: String? = null,
+    val isSuccess: Boolean = false,
+    val isLoading: Boolean = false,
+    val vehiculos: List<VehiculoDto> = emptyList()
+)
