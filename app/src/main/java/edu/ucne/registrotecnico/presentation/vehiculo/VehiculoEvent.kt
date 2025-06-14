548770a6cbbe98e9380ffec5cdc11ce32f594c24
jerony <jerony_cruz@ucne.edu.do>
2025-06-14 00:54:46 -0400

    Se creo el VehiculoEvent.

---
+++ app/src/main/java/edu/ucne/registrotecnico/presentation/vehiculo/VehiculoEvent.kt
@@ -0,0 +1,13 @@
+package edu.ucne.registrotecnico.presentation.vehiculo
+
+sealed interface VehiculoEvent {
+    data class VehiculoChange(val vehiculoId: Int):VehiculoEvent
+    data class DescripcionChange(val descripcion: String): VehiculoEvent
+    data class PrecioChange(val precio: Double): VehiculoEvent
+
+    data object PostVehiculo: VehiculoEvent
+    data object GetVehiculo: VehiculoEvent
+    data object Nuevo: VehiculoEvent
+    data object LimpiarErrorMessageDescripcion: VehiculoEvent
+    data object LimpiarErrorMessagePrecio: VehiculoEvent
+}
