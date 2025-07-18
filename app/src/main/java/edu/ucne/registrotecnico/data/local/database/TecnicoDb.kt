5abbe12e5478411b4c874edffe386f50941121e7
JeronyCruz <jerony_cruz@ucne.edu.do>
2025-07-17 23:54:41 -0400

    Agregando Entidad de Usuarios.

--- app/src/main/java/edu/ucne/registrotecnico/data/local/database/TecnicoDb.kt
+++ app/src/main/java/edu/ucne/registrotecnico/data/local/database/TecnicoDb.kt
@@ -8,11 +8,13 @@ import edu.ucne.registrotecnico.data.local.dao.MensajeDao
 import edu.ucne.registrotecnico.data.local.dao.PrioridadDao
 import edu.ucne.registrotecnico.data.local.dao.TecnicoDao
 import edu.ucne.registrotecnico.data.local.dao.TicketDao
+import edu.ucne.registrotecnico.data.local.dao.UsuarioDao
 import edu.ucne.registrotecnico.data.local.dao.VehiculoDao
 import edu.ucne.registrotecnico.data.local.entities.MensajeEntity
 import edu.ucne.registrotecnico.data.local.entities.PrioridadEntity
 import edu.ucne.registrotecnico.data.local.entities.TecnicoEntity
 import edu.ucne.registrotecnico.data.local.entities.TicketEntity
+import edu.ucne.registrotecnico.data.local.entities.UsuarioEntity
 import edu.ucne.registrotecnico.data.local.entities.VehiculoEntity
 import java.util.Date
 
@@ -34,9 +36,10 @@ class Converters {
         PrioridadEntity::class,
         TicketEntity::class,
         MensajeEntity::class,
-        VehiculoEntity::class
+        VehiculoEntity::class,
+        UsuarioEntity::class
     ],
-    version = 5,
+    version = 6,
     exportSchema = false
 )
 
@@ -47,4 +50,5 @@ abstract  class TecnicoDb : RoomDatabase(){
     abstract fun TicketDao(): TicketDao
     abstract fun MensajeDao(): MensajeDao
     abstract fun VehiculoDao(): VehiculoDao
+    abstract fun UsuarioDao(): UsuarioDao
 }
