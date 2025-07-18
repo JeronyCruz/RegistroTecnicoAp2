a62b6ce3f6c1d5e9bcb701590060644bf8f7269e
JeronyCruz <jerony_cruz@ucne.edu.do>
2025-07-17 23:55:12 -0400

    Agregando CRUD de Usuarios.

--- app/src/main/java/edu/ucne/registrotecnico/data/remote/RemoteDataSource.kt
+++ app/src/main/java/edu/ucne/registrotecnico/data/remote/RemoteDataSource.kt
@@ -1,11 +1,13 @@
 package edu.ucne.registrotecnico.data.remote
 
+import edu.ucne.registrotecnico.data.remote.dto.UsuarioDto
 import edu.ucne.registrotecnico.data.remote.dto.VehiculoDto
 import javax.inject.Inject
 
 class RemoteDataSource @Inject constructor(
     private val vehiculoinApi: VehiculoinApi
 ){
+
     suspend fun getVehiculos() = vehiculoinApi.getVehiculos()
 
     suspend fun getVehiculo(id: Int) = vehiculoinApi.getVehiculos(id)
@@ -15,4 +17,15 @@ class RemoteDataSource @Inject constructor(
     suspend fun saveVehiculo(vehiculoDto: VehiculoDto) = vehiculoinApi.saveVehiculo(vehiculoDto)
 
     suspend fun deleteVehiculo(idVehiculo: Int) = vehiculoinApi.deleteVehiculo(idVehiculo)
+
+    // Usuarios
+    suspend fun getUsuarios()= vehiculoinApi.getUsuarios()
+
+    suspend fun updateUsuario(usuarioDto: UsuarioDto)= vehiculoinApi.updateUsuario(usuarioDto)
+
+    suspend fun saveUsuario(usuarioDto: UsuarioDto)= vehiculoinApi.saveUsuario(usuarioDto)
+
+    suspend fun deleteUsuario(id: Int)= vehiculoinApi.deleteUsuario(id)
+
+    suspend fun getUsuario(id: Int)= vehiculoinApi.getUsuario(id)
 }
