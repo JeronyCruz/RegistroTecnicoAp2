2281bd0b4bee5a0b38ba9cf5744a4223a79888ef
JeronyCruz <jerony_cruz@ucne.edu.do>
2025-07-17 23:51:03 -0400

    Creando entidad Usuario.

---
+++ app/src/main/java/edu/ucne/registrotecnico/data/local/entities/Usuario.kt
@@ -0,0 +1,12 @@
+package edu.ucne.registrotecnico.data.local.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Usuarios")
+data class UsuarioEntity(
+    @PrimaryKey
+    val usuarioId: Int? = null,
+    val nombre: String = "",
+    val balance: Double = 0.0
+)
